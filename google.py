e8eefa91b7f55375e7f41d791e962978575dfc3f
Monish <monishkumar833@gmail.com>
2022-08-19 11:00:20 -0400

    changes

---
+++ google.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+PATH = "/Users/harishgundluru/Downloads/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://google.com")
+print(driver.title)
+
+search = driver.find_element(By.NAME, "q")
+search.click()
+time.sleep(2)
+search.send_keys("charles babbage")
+time.sleep(2)
+search.send_keys(Keys.RETURN)
+try:
+    about = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "kno-rdesc"))
+    )
+except:
+    driver.quit()
+about = driver.find_element(By.CLASS_NAME, "kno-rdesc")
+time.sleep(2)
+print(about.text)
+driver.quit()
