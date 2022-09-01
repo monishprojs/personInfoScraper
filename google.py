af4d212ff5b1812d3409ccd60878aead6a742e7e
Monish <monishkumar833@gmail.com>
2022-09-01 13:03:12 -0500

    doesnt open window anymore, got rid of time.sleep to make faster

--- google.py
+++ google.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
@@ -9,16 +10,18 @@ import time
 person = "isaac newton"
 
 PATH = "/Users/harishgundluru/Downloads/chromedriver"
-driver = webdriver.Chrome(PATH)
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(PATH, options=chrome_options)
 
 driver.get("https://google.com")
 print(driver.title)
 
 search = driver.find_element(By.NAME, "q")
 search.click()
-time.sleep(2)
 search.send_keys(person)
-time.sleep(2)
 search.send_keys(Keys.RETURN)
 try:
     about = WebDriverWait(driver, 10).until(
